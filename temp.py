9362ce48071cc7803d78ce6306750b386168a873
Cédric Maigrot <maigrot.cedric@gmail.com>
2021-04-24 10:47:26 +0200

    VERSION 2.0.0 (base)

--- temp.py
+++ temp.py
@@ -6,7 +6,7 @@ from io import StringIO
 import pandas as pd
 df_members = pd.read_csv("outputs/members.csv")
 def get_discriminator(author):
-	return author.split("#")[-1]
+   return author.split("#")[-1]
 df_quizz = pd.read_csv("outputs/charts/classement.csv")
 df_quizz['discriminator'] = df_quizz.apply(lambda x: get_discriminator(x['Username']), axis=1)
 df_members['discriminator'] = df_members['discriminator'].astype(int)
@@ -17,7 +17,7 @@ pos = [[252,52], [92,92], [412,92]]
 newsize = (100, 100)
 urls = list()
 for id, row in df.head(3).iterrows():
-	urls.append(row['avatar_url'])
+   urls.append(row['avatar_url'])
 
 
 from PIL import Image, ImageDraw, ImageFilter
@@ -29,19 +29,18 @@ headers={'User-Agent':user_agent,}
 
 import requests
 for i in range(3):
-	print(urls[i])
-	with open('pic1.png', 'wb') as handle:
-		response = requests.get(urls[i], stream=True)
-		if not response.ok:
-			print (response)
-		for block in response.iter_content(1024):
-			if not block:
-				break
-			handle.write(block)
+   print(urls[i])
+   with open('pic1.png', 'wb') as handle:
+      response = requests.get(urls[i], stream=True)
+      if not response.ok:
+         print (response)
+      for block in response.iter_content(1024):
+         if not block:
+            break
+         handle.write(block)
 
-	im = Image.open('pic1.png')
-	im = im.resize(newsize)
-	bg.paste(im, (pos[i][0],pos[i][1]))
+   im = Image.open('pic1.png')
+   im = im.resize(newsize)
+   bg.paste(im, (pos[i][0],pos[i][1]))
 
 bg.save('outputs/podium.png', quality=95)
-
